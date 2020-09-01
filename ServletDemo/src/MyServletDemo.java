3fef22b421c858b06d7b5f9760625b856f924de7
Sam Wolfe <49419226+samgwolfe12@users.noreply.github.com>
2020-09-01 00:15:05 -0400

    new functionality to GET name from form

--- ServletDemo/src/MyServletDemo.java
+++ ServletDemo/src/MyServletDemo.java
@@ -22,7 +22,25 @@ public class MyServletDemo extends HttpServlet {
 
       // Writing message to the web page
       PrintWriter out = response.getWriter();
-      out.println("<h1>" + mymsg + "</h1>");
+      String title = "Using GET Method to Read Form Data";
+      String docType =
+         "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+      
+      out.println(docType +
+    	         "<html>\n" +
+    	            "<head><title>" + title + "</title></head>\n" +
+    	            "<body bgcolor = \"#f0f0f0\">\n" +
+    	               "<h1 align = \"center\">" + title + "</h1>\n" +
+    	               "<ul>\n" +
+    	                  "  <li><b>First Name</b>: "
+    	                  + request.getParameter("first_name") + "\n" +
+    	                  "  <li><b>Last Name</b>: "
+    	                  + request.getParameter("last_name") + "\n" +
+    	               "</ul>\n" +
+    	            "</body>" +
+    	         "</html>"
+    	      );
+      
    }
 
    public void destroy() {
